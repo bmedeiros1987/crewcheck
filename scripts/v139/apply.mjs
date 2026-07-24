bff26cdbf0ea82bf873bfd333309063efea92d09
bmedeiros1987 <bmedeiros1987@gmail.com>
2026-07-23 23:20:47 -0300

    Ativar lembretes operacionais da v14.3.22

--- scripts/v139/apply.mjs
+++ scripts/v139/apply.mjs
@@ -71,3 +71,4 @@ await import('../v1423/apply.mjs');
 await import('../v1424/apply.mjs');
 await import('../v1425/apply.mjs');
 await import('../v14312/apply.mjs');
+await import('../v14322/apply.mjs');
