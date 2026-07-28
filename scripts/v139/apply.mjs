a7528d9b669b74d638a99b8270ca48fe3124f57b
bmedeiros1987 <bmedeiros1987@gmail.com>
2026-07-28 12:34:44 -0300

    build(v14346): chain fresh location hotfix

--- scripts/v139/apply.mjs
+++ scripts/v139/apply.mjs
@@ -100,3 +100,4 @@ await import('../v14342/apply.mjs');
 await import('../v14343/apply.mjs');
 await import('../v14344/apply.mjs');
 await import('../v14345/apply.mjs');
+await import('../v14346/apply.mjs');
