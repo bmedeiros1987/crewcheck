d397ec105a39a5e2e15ae8083e15b63a1721349b
bmedeiros1987 <bmedeiros1987@gmail.com>
2026-07-30 03:59:11 -0300

    chore(prepare): aplicar compatibilidade final da v14.3.57

--- scripts/v139/apply.mjs
+++ scripts/v139/apply.mjs
@@ -112,3 +112,4 @@ await import('../v14354/apply.mjs');
 await import('../v14355/apply.mjs');
 await import('../v14356/apply.mjs');
 await import('../v14357/apply.mjs');
+await import('../v14357/compatibility.mjs');
