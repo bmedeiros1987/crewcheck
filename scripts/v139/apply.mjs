4525a31ad218068c541e5f01608009c3b7ab8d0a
bmedeiros1987 <bmedeiros1987@gmail.com>
2026-08-02 17:17:29 -0300

    chore(prepare): apply roster fingerprint parity hotfix

--- scripts/v139/apply.mjs
+++ scripts/v139/apply.mjs
@@ -118,3 +118,4 @@ await import('../v14359/apply.mjs');
 await import('../v14365/apply.mjs');
 await import('../v14367/apply.mjs');
 await import('../v14371/apply.mjs');
+await import('../v14372/apply.mjs');
