87e3f0ed8da2ba319b9eaef6a05542151b0bae07
bmedeiros1987 <bmedeiros1987@gmail.com>
2026-08-02 01:26:11 -0300

    chore(roster): chain MCK/PHB hotfix

--- scripts/v139/apply.mjs
+++ scripts/v139/apply.mjs
@@ -116,3 +116,4 @@ await import('../v14357/compatibility.mjs');
 await import('../v14358/apply.mjs');
 await import('../v14359/apply.mjs');
 await import('../v14365/apply.mjs');
+await import('../v14367/apply.mjs');
