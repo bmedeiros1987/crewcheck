b4115b3b0e01a7ef479737da7227aa56c114f95f
bmedeiros1987 <bmedeiros1987@gmail.com>
2026-08-05 13:21:12 -0300

    feat(whatsapp): include v14.3.82 source preparation

--- scripts/v139/apply.mjs
+++ scripts/v139/apply.mjs
@@ -127,4 +127,5 @@ await import('../v14377/apply.mjs');
 await import('../v14378/apply.mjs');
 await import('../v14380/apply.mjs');
 await import('../v14381/apply.mjs');
+await import('../v14382/apply.mjs');
 await import('../ci/sync-canonical-manual.mjs');
