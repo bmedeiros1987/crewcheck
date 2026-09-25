337f613ffd7996d4e0d8acbf4eba751ebaa5006d
bmedeiros1987 <bmedeiros1987@gmail.com>
2026-09-24 22:38:38 -0300

    feat(auto): add CrewCheckCarAppService.java

---
+++ android-wrapper/auto/src/main/java/com/crewcheck/auto/CrewCheckCarAppService.java
@@ -0,0 +1,29 @@
+package com.crewcheck.auto;
+
+import android.content.pm.ApplicationInfo;
+
+import androidx.annotation.NonNull;
+import androidx.car.app.CarAppService;
+import androidx.car.app.Session;
+import androidx.car.app.SessionInfo;
+import androidx.car.app.validation.HostValidator;
+
+public final class CrewCheckCarAppService extends CarAppService {
+    @Override
+    @NonNull
+    public Session onCreateSession(@NonNull SessionInfo sessionInfo) {
+        return new CrewCheckCarSession();
+    }
+
+    @Override
+    @NonNull
+    public HostValidator createHostValidator() {
+        if ((getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0) {
+            return HostValidator.ALLOW_ALL_HOSTS_VALIDATOR;
+        }
+
+        return new HostValidator.Builder(getApplicationContext())
+                .addAllowedHosts(androidx.car.app.R.array.hosts_allowlist_sample)
+                .build();
+    }
+}
